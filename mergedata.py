715a4be9c429eaa33eb36031a11a3d101b861d3a
gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2023-01-25 11:57:07 -0300

    .
    Signed-off-by: gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>

--- mergedata.py
+++ mergedata.py
@@ -4,6 +4,10 @@ import os
 import re
 import logging
 
+#Warnings: Possui uma série de funções e comandos para tratamento de mensagens de avisos e alertas do Python
+import warnings
+warnings.filterwarnings("ignore")
+
 # gerando log
 logging.basicConfig(level=logging.INFO, filename="mergedata.log", encoding='utf-8', format="%(asctime)s - %(levelname)s - %(message)s")
 
@@ -13,7 +17,6 @@ Dados = pd.DataFrame()
 # Diretórios
 diretorio_estabelecimentos = r'base_csv_estabelecimentos/'
 all_files_estabelecimentos = list(filter(lambda x: '.csv' in x, os.listdir(diretorio_estabelecimentos)))
-
 
 
 all_files_estabelecimentos 
@@ -30,11 +33,13 @@ municipios = pd.read_csv('Municipios\municipios.csv', sep=';', names=['MUNICIPIO
 
 # Iterando sobre os estabelecimentos
 for file_estabelecimento in all_files_estabelecimentos:
+    logging.info(f"Lendo arquivo {file_estabelecimento}")
+    print(f"Lendo arquivo {file_estabelecimento}")
     # DataFrame Vazio
     dados = pd.DataFrame()
 
 
-    d_estabelecimento = pd.read_csv(f'{diretorio_estabelecimentos}{file_estabelecimento}', sep=',', dtype=dtypes_ESTABELE)
+    d_estabelecimento = pd.read_csv(f'{diretorio_estabelecimentos}{file_estabelecimento}', sep=';', dtype=dtypes_ESTABELE)
     #print(d_estabelecimento.columns)
     d_estabelecimento['CNPJ'] = d_estabelecimento[['CNPJ_BASE', 'CNPJ_ORDEM', 'CNPJ_DV']].apply(lambda x: ''.join(x), axis=1)
     d_estabelecimento['CNPJ_BASE'] = pd.to_numeric(d_estabelecimento['CNPJ_BASE'], downcast='integer')
@@ -54,15 +59,16 @@ for file_estabelecimento in all_files_estabelecimentos:
         merged_data = pd.merge(merged_data, municipios, on='MUNICIPIO')
         merged_data['RUA'] = merged_data['TIPO_LOGRADOURO'] +' '+ merged_data['LOGRADOURO']
         merged_data.rename(columns={'UF':'ESTADO'}, inplace=True)
-        merged_data = merged_data[['CNPJ', 'RAZAO_SOCIAL', 'NOME_FANTASIA','RUA','COMPLEMENTO', 'BAIRRO','CIDADE','ESTADO','CEP']]
+        merged_data = merged_data[['CNPJ', 'RAZAO_SOCIAL', 'NOME_FANTASIA','RUA', 'NUMERO','COMPLEMENTO', 'BAIRRO',
+            'CIDADE','ESTADO','CEP', 'TELEFONE', 'EMAIL', 'CNAE_PRINCIPAL','CNAE_DESCRICAO',]]
         
         dados = pd.concat([dados, merged_data], ignore_index=True)
         
     CAMADA1 = re.sub('base_csv_estabelecimentos','', file_estabelecimento)
     name_file = re.sub('["("")"?@|$|/|\|!,:%;"]','', CAMADA1)
-
-    dados.to_csv(f'merge_base/{name_file}', mode='w', index=False)
+    name_file_json = re.sub(".csv", '.json', name_file)
+    dados.to_csv(f'merge_base/{name_file}', mode='w', index=False, sep=';')
+    dados.to_json(f'json_base/{name_file_json}', orient='records')
     print(f"Concluído processo de extração dos dados do CNAE de {name_file}")
-
-
+    logging.info(f"Concluído processo de extração de {len(dados)} dados do CNAE de {name_file}")
 
