74561e2723fa212c63b174e632ff5bc76a2e729f
gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2023-01-31 14:06:23 -0300

    .
    Signed-off-by: gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>

--- enriquecimento.py
+++ enriquecimento.py
@@ -3,15 +3,15 @@ import pandas as pd
 import json
 import re
 import os
-from googletrans import Translator
+import translators.server as tss
 import concurrent.futures
-
-
 import requests
+from requests.adapters import HTTPAdapter
+
 
 def get_lat_long(cep:str):
     """Busca Latitude e Longitude de um CEP válido informado"""
-    url = f"https://api.postmon.com.br/v1/cep/{cep}"
+    url = f'https://viacep.com.br/ws/{cep}/json/'
     response = requests.get(url)
     if response.status_code == 200:
         location = response.json()
@@ -19,9 +19,9 @@ def get_lat_long(cep:str):
         lng = location.get("longitude")
         return lat, lng
     else:
-        return None
+        return -0, 0
 
-def main(cep_list):
+def thread_get_lat_long(cep_list):
     """Implanta o multithreading na função `get_lat_long`"""
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = [executor.submit(get_lat_long, cep) for cep in cep_list]
@@ -35,10 +35,24 @@ def main(cep_list):
     return lat_longs
 
 
-def traduzir(texto, dest_lang='en'):
+def traduzir(texto):
     """ Traduz um texto informado do Português para o Inglês"""
-    tradutor = Translator(dest_lang=dest_lang)
-    return tradutor.translate(texto).text
+    traducao = tss.google(texto, from_language='pt', to_language='en')
+    return traducao
+
+dict_comparador ={
+    'CATEGORY_(CNAE)' : ['Serviços ambulantes de alimentação',
+            'Restaurantes e similares',
+            'Lanchonetes casas de chá de sucos e similares',
+            'Bares e outros estabelecimentos especializados em servir bebidas sem entretenimento',
+            'Bares e outros estabelecimentos especializados em servir bebidas com entretenimento'],
+    'CNAE_TRADUCAO' : []
+    }
+for cnae in dict_comparador['CATEGORY_(CNAE)']:
+    dict_comparador['CNAE_TRADUCAO'].append(traduzir(cnae))
+
+comparador = pd.DataFrame(dict_comparador)
+
 
 # Diretórios
 diretorio = r'merge_base/'
@@ -49,7 +63,7 @@ for file in all_files:
 
     dados = pd.read_csv(f"{diretorio}{file}", sep=';')
     lista_cep = dados['CEP'].tolist()
-    lat_longs = main(lista_cep)
+    lat_longs = thread_get_lat_long(lista_cep)
     dados['LATITUDE'], dados['LONGITUDE'] = zip(*lat_longs)
     dados['SITE'] = "www" + dados['NOME_FANTASIA'].str.lower().replace(" ", "", regex=True) + ".com"
     dados['FACEBOOK'] = "https://pt-br.facebook.com/" + dados['NOME_FANTASIA'].str.lower().replace(" ","",regex=True)
@@ -67,7 +81,9 @@ for file in all_files:
     data.rename(columns={'CNPJ':"EIN (CNPJ)", 'RAZAO_SOCIAL':'CORPORATE_NAME', 'NOME_FANTASIA':'TRADING NAME', 'RUA':'STREET', 'NUMERO':'ADDRESS_NUMBER', 'COMPLEMENTO':'ADDRESS_COMPLEMENT',
        'BAIRRO':'DISTRICT', 'CIDADE':'CITY', 'ESTADO':'STATE', 'CEP':'ZIP_CODE', 'TELEFONE':'PHONE_NUMBER', 'HORARIO_FUNCIONAMENTO':'OPENING_HOURS', 'OPCOES_DE_SERVICO':'SERVICE_OPTIONS', 'CNAE_DESCRICAO':'CATEGORY_(CNAE)'}, inplace=True)
 
-    data['CATEGORY_(CNAE)'] = data['CATEGORY_(CNAE)'].str.replace(f"{data['CATEGORY_(CNAE)'].str}", f"{traduzir(data['CATEGORY_(CNAE)'].str)}", regex=True)
-    
-    dados.to_csv(f'br_base_json/{file}', index=False, sep=';')
+    #data['CATEGORY_(CNAE)'] = data['CATEGORY_(CNAE)'].str.replace(f"{data['CATEGORY_(CNAE)'].str}", f"{traduzir(data['CATEGORY_(CNAE)'].str)}", regex=True)
+    # Tratando os dados para disposição
+    data = pd.merge(data, comparador, on='CATEGORY_(CNAE)')
+    data['CATEGORY_(CNAE)'] = data['CNAE_TRADUCAO']
+    dados.to_csv(f'br_base/{file}', index=False, sep=';')
     data.to_csv(f'en_base/{file}', index=False, sep=';')
