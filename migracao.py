12b93d5b262127a2612dd7644a064d2f88517185
gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2023-02-01 11:30:17 -0300

    .
    Signed-off-by: gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>

--- migracao.py
+++ migracao.py
@@ -17,7 +17,9 @@ en_arquivos = [f for f in os.listdir(en_diretorio) if f.endswith('.csv')]
 # Loop por todos os br_arquivos
 for br_arquivo in br_arquivos:
     # Lê o arquivo para um dataframe
-    dados = pd.read_csv(f'{br_diretorio}{br_arquivo}')
+    dados = pd.read_csv(f'{br_diretorio}{br_arquivo}', sep=';', low_memory=False)
+    dados['NOME_FANTASIA'].replace('--empty--','', regex=True)
+    #print(dados.info())
     
     # Salva o dataframe como uma tabela no SQLite
     dados.to_sql('br_base', br_conn, if_exists='append', index=False)
@@ -31,7 +33,8 @@ br_conn.close()
 # Loop por todos os en_arquivos
 for en_arquivo in en_arquivos:
     # Lê o arquivo para um dataframe
-    data = pd.read_csv(f'{en_diretorio}{en_arquivo}')
+    data = pd.read_csv(f'{en_diretorio}{en_arquivo}', sep=';', low_memory=False)
+    data['TRADING NAME'].replace('--empty--','', regex=True)
     
     # Salva o dataframe como uma tabela no SQLite
     data.to_sql('en_base', en_conn, if_exists='append', index=False)
