dbb7bf9cfd00768dad8e8f7fe24a5276703cb19f
gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2023-02-02 13:40:02 -0300

    .
    Signed-off-by: gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>

--- aplicacao/index.js
+++ aplicacao/index.js
@@ -1,7 +1,7 @@
 // GET all data from the database
 async function getCNPJs() {
     try {
-      const response = await axios.get('http://localhost:3000/estabelecimentos/get/all');
+      const response = await axios.get('http://localhost:8000/estabelecimentos/get/all');
       console.log(response.data.cnpjs);
     } catch (error) {
       console.error(error);
@@ -11,7 +11,7 @@ async function getCNPJs() {
   // GET data by CNPJ
   async function getCNPJ(cnpj) {
     try {
-      const response = await axios.get(`http://localhost:3000/estabelecimentos/get/cnpj=${cnpj}`);
+      const response = await axios.get(`http://localhost:8000/estabelecimentos/get/cnpj=${cnpj}`);
       console.log(response.data.cnpj);
     } catch (error) {
       console.error(error);
@@ -21,7 +21,7 @@ async function getCNPJs() {
   // POST data to the database
   async function addCNPJ(cnpjData) {
     try {
-      const response = await axios.post('http://localhost:3000/estabelecimentos/insert/cnpj', cnpjData);
+      const response = await axios.post('http://localhost:8000/estabelecimentos/insert/cnpj', cnpjData);
       console.log(response.data.message);
     } catch (error) {
       console.error(error);
@@ -31,7 +31,7 @@ async function getCNPJs() {
   // PUT data to the database
   async function updateCNPJ(cnpj, cnpjData) {
     try {
-      const response = await axios.put(`http://localhost:3000/estabelecimentos/update/cnpj=${cnpj}`, cnpjData);
+      const response = await axios.put(`http://localhost:8000/estabelecimentos/update/cnpj=${cnpj}`, cnpjData);
       console.log(response.data.message);
     } catch (error) {
       console.error(error);
@@ -41,7 +41,7 @@ async function getCNPJs() {
   // DELETE data from the database
   async function deleteCNPJ(cnpj) {
     try {
-      const response = await axios.delete(`http://localhost:3000/estabelecimentos/delete/cnpj=${cnpj}`);
+      const response = await axios.delete(`http://localhost:8000/estabelecimentos/delete/cnpj=${cnpj}`);
       console.log(response.data.message);
     } catch (error) {
       console.error(error);
