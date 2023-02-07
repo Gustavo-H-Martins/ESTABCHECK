cb4073006e22bd639ba197f072ba24adeb72a8bf
gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2023-02-07 09:24:45 -0300

    .
    Signed-off-by: gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>

--- aplicacao/chamadas_api.js
+++ aplicacao/chamadas_api.js
@@ -1,16 +1,28 @@
 function atualizaPainel(){
-  var url = 'http://localhost:3000/estabelecimentos/get/all'
+  var url = 'http://localhost:8000/estabelecimentos/get/all'
   fetch(url)
   .then(function (res) {
     return res.json();
   }).then(function (apiData) {
-    //renderizaDadosNaTabela(apiData, 50);
-    renderizarTabela(apiData)
+    renderizaDadosNaTabela(apiData, 50);
+
   });
 }
+const tabela = document.getElementById('cnpjs');
+const corpoTabela = document.getElementById('cnpjs');
+const anterior = document.getElementById('anterior');
+const proximo = document.getElementById('proximo');
+
+const tamanhoPagina = 50;
+let paginaAtual = 0;
 
 function renderizaDadosNaTabela(dados, chunkSize) {
-  const tabela = document.getElementById("cnpjs");
+  
+  const inicio = paginaAtual * tamanhoPagina;
+  const fim = inicio + tamanhoPagina;
+  const paginaDados = dados.slice(inicio, fim);
+
+  const tabela = document.getElementById("corpo-tabela");
   for (let i = 0; i < dados.length; i += chunkSize) {
     let chunk = dados.slice(i, i + chunkSize);
     chunk.forEach(dado => {
@@ -25,44 +37,17 @@ function renderizaDadosNaTabela(dados, chunkSize) {
   }
 }
 
-const tabela = document.getElementById('cnpjs');
-const corpoTabela = document.getElementById('corpo-tabela');
-const anterior = document.getElementById('anterior');
-const proximo = document.getElementById('proximo');
-
-const tamanhoPagina = 50;
-let paginaAtual = 0;
-
-function renderizarTabela(dados){
-  const corpoTabela = document.getElementById("cnpjs");
-  corpoTabela.innerHTML = '';
-
-  const inicio = paginaAtual * tamanhoPagina;
-  const fim = inicio + tamanhoPagina;
-  const paginaDados = dados.slice(inicio, fim);
-
-  paginaDados.forEach((dado) => {
-    const linha = document.createElement('tr');
-    Object.values(dado).forEach((value) => {
-      let celula = document.createElement('td');
-      celula.innerText = value;
-    });
-    corpoTabela.appendChild(linha);
-  });
-}
-
 anterior.addEventListener('click', () =>{
   if (paginaAtual > 0) {
     paginaAtual--;
-    renderizarTabela(dados);
+    renderizaDadosNaTabela(apiData, 50);
   }
 });
 
 proximo.addEventListener('click', () => {
   if (paginaAtual < Math.ceil(dados.length / tamanhoPagina) -1) {
     paginaAtual ++;
-    renderizarTabela(dados);
+    renderizaDadosNaTabela(apiData, 50);
   }
 });
 
-//renderizarTabela()
