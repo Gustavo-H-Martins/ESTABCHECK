e4eaa02fcfad2c90e7a312b6f7e9df725e06a770
gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2023-02-24 16:47:29 -0300

    .
    Signed-off-by: gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>

--- utilitarios/backup_limpeza.py
+++ utilitarios/backup_limpeza.py
@@ -79,12 +79,14 @@ def backup_limpeza_simples(pasta:str = os.listdir('.')[0],
     """
     # Define o diretório de trabalho
     diretorio = pasta
+    if diretorio[-1] != "/" or diretorio[-1] != "\ ".strip():
+        diretorio = f'{diretorio}/'
 
     # Define o nome do arquivo zip a ser criado
     nome_zip = nome_zipado
 
     # Cria o arquivo zip
-    with zipfile.ZipFile(nome_zip, mode='w',compression=zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(f'{diretorio}{nome_zip}', mode='w',compression=zipfile.ZIP_DEFLATED) as zipf:
         # Percorre o diretório em busca de arquivos csv que atendem aos critérios de data de modificação
         for arquivo in os.listdir(diretorio):
             if arquivo.endswith(extensao):
