bd4373a0b3f8bbfcbaec6da45d42ded0a322ccd4
gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2023-04-24 11:21:16 -0300

    .
    Signed-off-by: gustavolopesmartins <70769813+Gustavo-H-Martins@users.noreply.github.com>

--- utilitarios/busca_geolocalizacao.py
+++ utilitarios/busca_geolocalizacao.py
@@ -11,14 +11,17 @@ municipios_lat_lon = current_dir.replace("utilitarios", r"auxiliares/MUNICIPIOS_
 with open(municipios, "r", encoding="utf-8") as m:
     geo_municipios = m.readlines()[1:]
     m.close()
-
+# criando o dataframe vazio para fazer a concatenação dos dados de retorno
 dados = pd.DataFrame()
 for municipio in geo_municipios:
     # usando os separadores para pegar o campo que quero
     partes_municipio = municipio.split(";")
     
+    # chamando e já criando o dataframe com o retorno da função get_geolocalizacao
     base = pd.DataFrame(get_geolocalizacao(cod_municipio=int(partes_municipio[0])))
-
+    # concatenando com o dataframe vazio criado lá em cima
     dados = pd.concat([dados, base])
+# retirando eventuais duplicados
 dados.drop_duplicates(inplace=True)
+# salvando em um CSV para não criar caso por hora
 dados.to_csv(municipios_lat_lon, index=False)
